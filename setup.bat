b521fff57d621cd7e4896eab5d3aa8fdae657a66
Kelimm <166009398+Kelimm@users.noreply.github.com>
2025-05-05 22:32:48 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -21,18 +21,9 @@ echo cd /d "%%~dp0" >> launch_server.bat
 echo call myenv\Scripts\activate.bat >> launch_server.bat
 echo python serveur.py >> launch_server.bat
 
-:: Créer launch_server.vbs
-echo Set WshShell = CreateObject("WScript.Shell") > launch_server.vbs
-echo WshShell.Run chr(34) ^& "%%~dp0launch_server.bat" ^& chr(34), 0 >> launch_server.vbs
-echo Set WshShell = Nothing >> launch_server.vbs
-
-:: Créer la tâche planifiée silencieuse
-echo.
-echo Creating silent scheduled task to run Flask server at login...
-
 schtasks /Create ^
  /TN "YT-Flask-Server" ^
- /TR "\"%~dp0launch_server.vbs\"" ^
+ /TR "powershell.exe -WindowStyle Hidden -Command \"Start-Process '%~dp0launch_server.bat' -WindowStyle Hidden\"" ^
  /SC ONLOGON ^
  /RL HIGHEST ^
  /F
